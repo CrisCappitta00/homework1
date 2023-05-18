3637f2bd31878a71538169b98decbd884f729f33
Cristina <cppcst00e48f943w@studium.unict.it>
2023-05-18 22:05:03 +0200

    Modificato

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Ciao Mondo</h1>
+    <header>
+        <h1>Benvenuto su Mondo Anime</h1>
+        <p>
+         In questo sito puoi trovare tutto ciò che riguarda gli anime: 
+         <br> manga, novel, anime, film e 
+         potrai sceglierne quanti ne vorrai, metterli nei preferiti o aggiungerli al carrello. 
+         <br>Vi sono diversi stili e categorie che spaziano dal fantasy, all' avventura, fino al comico.
+         <br>Cosa aspetti, registrati anche tu! Se invece sei un utente già registrato mi raccomando continua ad usare il mio sito! 
+        </p>
+    </header>
 </body>
 </html>
