82fd744fc850e35ba5a5247838689243d40922a1
Cristina <cppcst00e48f943w@studium.unict.it>
2023-05-18 20:45:02 +0200

    Mod

--- index.php
+++ index.php
@@ -3,13 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homework1</title>
-    <link rel="stylesheet" type="text/css" href="hmw1.css">
+    <title>Document</title>
 </head>
 <body>
-    <!--Creazione Menù nella parte superiore-->
-    <header>
-        
-    </header>
+    <h1>Ciao Mondo</h1>
 </body>
 </html>
