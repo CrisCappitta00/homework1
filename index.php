e8f1c2aaa973eaa86a797727c3aaa67ffeadce5f
Cristina <cppcst00e48f943w@studium.unict.it>
2023-05-19 21:50:29 +0200

    Mod

--- index.php
+++ index.php
@@ -7,15 +7,26 @@
     <title>Schermata iniziale</title>
 </head>
 <body>
+   
     <!--Creazione schermata login-->
     <header>
         <h1>Benvenuto su Mondo Anime</h1>
+        <img src="Anime.png" alt="Raffigura gli anime"></img>
         <p>
          In questo sito puoi trovare tutto ciò che riguarda gli anime: 
          <br> manga, novel, anime, film e 
          potrai sceglierne quanti ne vorrai, metterli nei preferiti o aggiungerli al carrello. 
          <br>Vi sono diversi stili e categorie che spaziano dal fantasy, all' avventura, fino al comico.
          <br>Cosa aspetti, registrati anche tu! Se invece sei un utente già registrato mi raccomando continua ad usare il mio sito! 
+        <?php 
+        $variabile = 'Ciao Mondo Bello';
+        function saluta(){
+            global $variabile;
+            $temp = $variabile;
+            return $temp;
+        }
+        echo '<h1 id="red">'.saluta().'</h1>'
+        ?>
         </p>
     </header>
 </body>
